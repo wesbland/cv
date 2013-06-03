2126b56f1a0913b321f864c6da117d6490fb71c9
Wesley Bland <wbland@mcs.anl.gov>
2013-06-03 10:20:30 -0500

    Update the bibtex generation.

--- update_website.sh
+++ update_website.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+export TMPDIR=.
 make
 mv cv.pdf wesley_bland.pdf
-bibtex2html -nokeys -d -r -s acm -css website/style.css -noheader -nofooter -nodoc -nokeywords papers.bib
+bibtex2html -nokeys -d -r -s acm -css website/css/style.css -noheader -nofooter -nodoc -nokeywords papers.bib
 scp wesley_bland.pdf papers.html papers_bib.html login1.mcs.anl.gov:public_html/
+make clean
+#rm wesley_bland.pdf papers.html papers_bib.html
