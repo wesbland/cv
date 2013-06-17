58a3bff137c73af23b9ca019e0a3c24418eecae8
Wesley Bland <wbland@mcs.anl.gov>
2013-06-17 16:52:09 -0500

    Add a tutorials section.

--- update_website.sh
+++ update_website.sh
@@ -3,7 +3,11 @@
 export TMPDIR=.
 make
 mv cv.pdf wesley_bland.pdf
-bibtex2html -nokeys -d -r -s acm -css website/css/style.css -noheader -nofooter -nodoc -nokeywords papers.bib
+bibtex2html -nokeys -d -r -s acm \
+    -css website/css/style.css \
+    -noheader -nofooter -nodoc -nokeywords \
+    -e june13mpi \
+    papers.bib
 scp wesley_bland.pdf papers.html papers_bib.html login1.mcs.anl.gov:public_html/
 make clean
 #rm wesley_bland.pdf papers.html papers_bib.html
